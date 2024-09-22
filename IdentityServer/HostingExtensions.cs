b06356d5949bdd91c14fadf33abea8e0072d7476
Pawel <pawel.smolik@gmail.com>
2024-09-22 20:08:49 +0200

    Add EntityFramework

--- IdentityServer/HostingExtensions.cs
+++ IdentityServer/HostingExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityServer
 {
@@ -6,13 +10,35 @@ namespace IdentityServer
     {
         public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
         {
+            var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+            SqlConnection conn = new SqlConnection(
+                new SqlConnectionStringBuilder()
+                {
+                    DataSource = ".",
+                    InitialCatalog = "IdentityServer",
+                    UserID = "sa",
+                    Password = "1qaz@WSX",
+                    TrustServerCertificate = true
+                }.ConnectionString
+            );
+
             // uncomment if you want to add a UI
             builder.Services.AddRazorPages();
             builder.Services.AddIdentityServer()
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients)
-                .AddInMemoryApiResources(Config.GetApiResource)
+                //.AddInMemoryApiScopes(Config.ApiScopes)
+                //.AddInMemoryClients(Config.Clients)
+                //.AddInMemoryApiResources(Config.GetApiResource)
                 .AddTestUsers(Config.Users)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(conn,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(conn,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
                 .AddProfileService<CustomProfileService>();
                 
 
@@ -60,7 +86,56 @@ namespace IdentityServer
             
             app.MapRazorPages().RequireAuthorization();
 
+            InitializeDatabase(app);
+
             return app;
+        }
+
+        private static void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+                /*
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetApiResource)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+                */
+
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var resource in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApiResource)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
